68fe0624922f5be0848d45030200d3d850bd58b2
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-24 11:22:19 -0300

    fix: Repo configuration

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,18 +1,48 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
+
 import { BooksService } from './books.service';
+import { BookDto } from './dto/book.dto';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Get()
-  findAll() {
-    return this.booksService.findAll();
+  findAll(@Req() request: Request) {
+    return this.booksService.findAll(request);
   }
 
   @Get(':bookId')
   findOne(@Param('bookId', ParseIntPipe) bookId: number) {
-    console.log(bookId);
     return this.booksService.findOne(bookId);
+  }
+
+  @Post()
+  create(@Body() newBook: BookDto) {
+    return this.booksService.create(newBook);
+  }
+
+  @Patch(':bookId')
+  update(
+    @Param('bookId', ParseIntPipe) bookId: number,
+    @Body() newBook: BookDto,
+  ) {
+    return this.booksService.update(bookId, newBook);
+  }
+
+  @Delete(':bookId')
+  remove(@Param('bookId', ParseIntPipe) bookId: number) {
+    return this.booksService.delete(bookId);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,12 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Request } from 'express';
+
+import { BookDto } from './dto/book.dto';
+import { Book } from './interfaces/book.interface';
 
 @Injectable()
 export class BooksService {
-  findAll(): any {
-    return 'Obtener lista de libros';
+  books: Book[] = [
+    {
+      id: 1,
+      title: 'Una historia de España',
+      genre: 'Historia',
+      description:
+        'Un relato ameno, personal, a ratos irónico, pero siempre único, de nuestra accidentada historia a través de los siglos. Una obra concebida por el autor para, en palabras suyas, «divertirme, releer y disfrutar; un pretexto para mirar atrás desde los tiempos remotos hasta el presente, reflexionar un poco sobre ello y contarlo por escrito de una manera poco ortodoxa.',
+      author: 'Arturo Pérez-Reverte',
+      publisher: 'Alfaguara',
+      pages: 256,
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/41%2B-e981m1L._SX311_BO1,204,203,200_.jpg',
+    },
+    {
+      id: 2,
+      title: 'Historia de España contada para escépticos',
+      genre: 'Historia',
+      description:
+        'Como escribe el autor, no pretende ser veraz, justa y desapasionada, porque ninguna historia lo es. No está hecha para halagar a reyes y gobernantes, ni pretende halagar a los banqueros, ni a la Conferencia Episcopal, ni al colectivo gay.',
+      author: 'Juan Eslava Galán',
+      publisher: 'Booket',
+      pages: 592,
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/51IyZ5Mq8YL._SX326_BO1,204,203,200_.jpg',
+    },
+  ];
+
+  findAll(request: Request): Book[] {
+    return this.books;
   }
 
-  findOne(bookId: number): any {
-    return 'Devuelve información sobre un libro específico';
+  findOne(bookId: number): Book {
+    const book = this.books.find((book) => book.id === bookId);
+    if (!book) {
+      throw new NotFoundException(`The book with id ${bookId} not exist.`);
+    }
+    return book;
+  }
+
+  create(newBook: BookDto): Book {
+    // const book = new Book();
+    // book.id = Math.max(...this.books.map((book) => book.id), 0) + 1;
+    // book.title = newBook.title;
+    // book.genre = newBook.genre;
+    // book.description = newBook.description;
+    // book.author = newBook.author;
+    // book.publisher = newBook.publisher;
+    // book.pages = newBook.pages;
+    // book.image_url = newBook.image_url;
+    const book = {
+      ...newBook,
+      //              [1,2]  === 1 2 0 // 2 + 1 = 3
+      //              [] === 0 + 1 = 1
+      id: Math.max(...this.books.map((book) => book.id), 0) + 1,
+    };
+    this.books.push(book);
+    return book;
+  }
+
+  update(bookId: number, newBook: BookDto): any {
+    return newBook;
+  }
+
+  delete(bookId: number): any {
+    return 'Eliminar un libro específico';
   }
 }
